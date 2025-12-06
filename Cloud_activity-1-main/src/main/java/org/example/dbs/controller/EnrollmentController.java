20af8cea720a3c5e89a5cfbe2d88f697710d5a83
Your Name <ak.tech106@gmail.com>
2025-12-06 19:03:04 +0530

    updated

--- Cloud_activity-1-main/src/main/java/org/example/dbs/controller/EnrollmentController.java
+++ Cloud_activity-1-main/src/main/java/org/example/dbs/controller/EnrollmentController.java
@@ -1,6 +1,7 @@
 package org.example.dbs.controller;
 
 import org.example.dbs.service.CourseService;
+import org.example.dbs.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,15 +12,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class EnrollmentController {
 
     private final CourseService courseService;
+    private final UserService userService;
 
-    public EnrollmentController(CourseService courseService) {
+    public EnrollmentController(CourseService courseService, UserService userService) {
         this.courseService = courseService;
+        this.userService = userService;
     }
 
     @GetMapping
     public String index(Model model) {
         model.addAttribute("courses", courseService.getAllCourses());
-        return "courses/list";
+        model.addAttribute("users", userService.getAllUsers());
+        return "enrollments/list";
     }
 
 }
